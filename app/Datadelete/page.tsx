422a19eb1ca3611ba7cf103dcf7f113475fed8cc
Tom <tbvanreijn@gmail.com>
2024-12-23 12:57:48 +0100

    data

--- app/Datadelete/page.tsx
+++ app/Datadelete/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Datadelete() {
+function Datadeletetion() {
   return <div>Data Deletion</div>;
 }
 
-export default Datadelete;
+export default Datadeletetion;
