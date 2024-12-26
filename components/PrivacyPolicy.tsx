2e69e2190345ca252d213021edc633e2e2c96b85
Tom <tbvanreijn@gmail.com>
2024-12-26 12:27:30 +0100

    last

---
+++ components/PrivacyPolicy.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const PrivacyPolicy = () => {
+  return (
+    <div>
+      <article className="prose m-12 text-black lg:prose-xl">
+        <h1 className="text-white">Privacy Policy</h1>
+        <p>
+          {" "}
+          1. Photoshoot4u.com is a web application that uses artificial
+          intelligence used to generate personalized main photos. This privacy
+          policy explains how we collect personal information, use, share and
+          protect when you use our app.
+        </p>{" "}
+        <p>
+          2. Data collection Personal data: We may do the following collect
+          personal information: Name Email address Uploaded photos Application
+          Usage Data Automatic Data Collection: We may automatically collect
+          data such as: IP address Application usage data Device information{" "}
+        </p>{" "}
+        <p>
+          3. Data use: We use your data to: Provide the app and maintain manage
+          your account inform you about updates and new features Analyze and
+          improve your use of the app Generate your main photos{" "}
+        </p>
+        <p>
+          4. Data sharing: We do not share your personal data with third
+          parties, unless: This is required by law. We need your consent is for
+          providing the app or managing your account{" "}
+        </p>
+        <p>
+          5. Data transfer: Your data may be transferred to and stored in
+          countries outside the European Economic Area (EEA). We take
+          appropriate measures to ensure that your data are protected no matter
+          where they are.
+        </p>
+        <p>
+          6.Update information: You can update your personal information or
+          delete it by contacting us at the livechat.
+        </p>{" "}
+        <p>
+          7. Given security: We implement appropriate technical and
+          organizational measures to protect your data against unauthorized
+          access, use, modification or destruction.  {" "}
+        </p>
+        <p>
+          8. Privacy Policy Changes: We may change this privacy policy from
+          update time to time. We will notify you changes via the app or by
+          email.  
+        </p>
+        <p>
+          9. Contact: For questions or comments about our privacy policy, please
+          contact us via live chat.
+        </p>
+      </article>
+    </div>
+  );
+};
---
+++ components/Terms.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const Terms = () => {
+  return (
+    <div>
+      <article className="prose text-black m-12 lg:prose-xl">
+        <h1 className="text-white">Terms</h1>
+        <p>
+          {" "}
+          1. Photoshoot4u.com is a web application that uses artificial
+          intelligence used to generate personalized main photos. This general
+          terms and conditions govern the use of the app and the relationship
+          between you and Photoshoot4u.com.
+        </p>{" "}
+        <p>
+          2. Acceptance: By using the app you agree to these general terms and
+          conditions.
+        </p>{" "}
+        <p>
+          3. Use of the app: You may only use the app for personal,
+          non-commercial purposes. You are not allowed to use the app in any way
+          that is harmful, unlawful or offensive.
+        </p>
+        <p>
+          4. Intellectual property: All intellectual property rights belong to
+          the app and its content belong to Photoshoot4u.com
+        </p>
+        <p>
+          5. Liability: Photoshoot4u.com is not liable for any damage you suffer
+          as a result of using the app.
+        </p>
+        <p>
+          6. Changes: We may change these terms and conditions from time to time
+          change. We will notify you of changes via the app or by email.
+        </p>{" "}
+        <p>
+          7. Termination: We may terminate your use of the App if you terminate
+          it violates general terms and conditions.
+        </p>
+        <p>
+          8. Governing law: These terms and conditions are governed by and
+          interpreted in accordance with Dutch law.
+        </p>
+        <p>
+          9. Contact: For questions or comments about our general terms and
+          conditions you can contact us via live chat.
+        </p>
+      </article>
+    </div>
+  );
+};
