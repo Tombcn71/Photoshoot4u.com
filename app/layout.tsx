ccb068efe4d01dac2d6c037490554c27cf09c1f9
Tom <tbvanreijn@gmail.com>
2024-12-26 13:43:38 +0100

    crisp

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,13 @@ import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
-
+import dynamic from "next/dynamic";
 export const metadata = {
   title: "Professional headshots without a photographer | Photoshoot4u",
   description:
     "Elevate your online presence with studio-quality AI headshots. No need to arrange a time-consuming photoshoot",
 };
+const CrispWithNoSSR = dynamic(() => import("../components/crisp"));
 
 export default function RootLayout({ children }: any) {
   return (
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: any) {
               <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
             }>
             <Navbar />
+            <CrispWithNoSSR />
           </Suspense>
         </section>
         <main className="flex flex-1 flex-col items-center py-16">
