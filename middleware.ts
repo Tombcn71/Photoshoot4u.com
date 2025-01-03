498faddb230c814660a371b2b54c5f521e04aad2
Tom <tbvanreijn@gmail.com>
2025-01-03 20:11:04 +0100

    nexti18

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,17 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
-
 import type { NextRequest } from "next/server";
 import { Database } from "./types/supabase";
+import { i18nRouter } from "next-i18n-router";
+import i18nConfig from "./i18nConfig";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
   await supabase.auth.getSession();
-  return res;
+  return i18nRouter(req, i18nConfig);
 }
+
+export const config = {
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
+};
