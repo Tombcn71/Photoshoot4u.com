de98eb6147f594e164f796e1fd415a75ecac375f
Tom <tbvanreijn@gmail.com>
2025-02-05 12:35:40 +0100

    fix

--- components/TrainModelZone.tsx
+++ components/TrainModelZone.tsx
@@ -1,163 +1,328 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Input } from "@/components/ui/input";
-import { X } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useCallback, useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { FaFemale, FaImages, FaMale, FaRainbow } from "react-icons/fa";
+import * as z from "zod";
+import { fileUploadFormSchema } from "@/types/zod";
+import { upload } from "@vercel/blob/client";
+import axios from "axios";
 
-interface ImageUploadFormProps {
-  packSlug: string;
-}
+type FormInput = z.infer<typeof fileUploadFormSchema>;
 
-export default function ImageUploadForm({ packSlug }: ImageUploadFormProps) {
+const stripeIsConfigured = process.env.NEXT_PUBLIC_STRIPE_IS_ENABLED === "true";
+
+export default function TrainModelZone({ packSlug }: { packSlug: string }) {
   const [files, setFiles] = useState<File[]>([]);
-  const [modelType, setModelType] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+  const router = useRouter();
 
-  const handleFileDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const droppedFiles = Array.from(e.dataTransfer.files);
-    handleFiles(droppedFiles);
+  const form = useForm<FormInput>({
+    resolver: zodResolver(fileUploadFormSchema),
+    defaultValues: {
+      name: "",
+      type: "man",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormInput> = () => {
+    trainModel();
   };
 
-  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFiles = Array.from(e.target.files || []);
-    handleFiles(selectedFiles);
-  };
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      const newFiles: File[] =
+        acceptedFiles.filter(
+          (file: File) => !files.some((f) => f.name === file.name)
+        ) || [];
 
-  const handleFiles = (newFiles: File[]) => {
-    const validTypes = newFiles.every(
-      (file) => file.type === "image/png" || file.type === "image/jpeg"
-    );
-    const validCount = newFiles.length + files.length <= 10;
+      // if user tries to upload more than 10 files, display a toast
+      if (newFiles.length + files.length > 10) {
+        toast({
+          title: "Too many images",
+          description:
+            "You can only upload up to 10 images in total. Please try again.",
+          duration: 5000,
+        });
+        return;
+      }
 
-    if (!validTypes) {
-      setErrorMessage("Only PNG and JPEG files are allowed.");
+      // display a toast if any duplicate files were found
+      if (newFiles.length !== acceptedFiles.length) {
+        toast({
+          title: "Duplicate file names",
+          description:
+            "Some of the files you selected were already added. They were ignored.",
+          duration: 5000,
+        });
+      }
+
+      // check that in total images do not exceed a combined 4.5MB
+      const totalSize = files.reduce((acc, file) => acc + file.size, 0);
+      const newSize = newFiles.reduce((acc, file) => acc + file.size, 0);
+
+      if (totalSize + newSize > 4.5 * 1024 * 1024) {
+        toast({
+          title: "Images exceed size limit",
+          description:
+            "The total combined size of the images cannot exceed 4.5MB.",
+          duration: 5000,
+        });
+        return;
+      }
+
+      setFiles([...files, ...newFiles]);
+
+      toast({
+        title: "Images selected",
+        description: "The images were successfully selected.",
+        duration: 5000,
+      });
+    },
+    [files]
+  );
+
+  const removeFile = useCallback(
+    (file: File) => {
+      setFiles(files.filter((f) => f.name !== file.name));
+    },
+    [files]
+  );
+
+  const trainModel = useCallback(async () => {
+    setIsLoading(true);
+    // Upload each file to Vercel blob and store the resulting URLs
+    const blobUrls = [];
+
+    if (files) {
+      for (const file of files) {
+        const blob = await upload(file.name, file, {
+          access: "public",
+          handleUploadUrl: "/astria/train-model/image-upload",
+        });
+        blobUrls.push(blob.url);
+      }
+    }
+
+    // console.log(blobUrls, "blobUrls");
+
+    const payload = {
+      urls: blobUrls,
+      name: form.getValues("name").trim(),
+      type: form.getValues("type"),
+      pack: packSlug,
+    };
+
+    // Send the JSON payload to the "/astria/train-model" endpoint
+    const response = await fetch("/astria/train-model", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    setIsLoading(false);
+
+    if (!response.ok) {
+      const responseData = await response.json();
+      const responseMessage: string = responseData.message;
+      console.error("Something went wrong! ", responseMessage);
+      const messageWithButton = (
+        <div className="flex flex-col gap-4">
+          {responseMessage}
+          <a href="/get-credits">
+            <Button size="sm">Get Credits</Button>
+          </a>
+        </div>
+      );
+      toast({
+        title: "Something went wrong!",
+        description: responseMessage.includes("Not enough credits")
+          ? messageWithButton
+          : responseMessage,
+        duration: 5000,
+      });
       return;
     }
 
-    if (!validCount) {
-      setErrorMessage("You can upload a maximum of 10 images.");
-      return;
-    }
+    toast({
+      title: "Model queued for training",
+      description:
+        "The model was queued for training. You will receive an email when the model is ready to use.",
+      duration: 5000,
+    });
 
-    setFiles((prevFiles) => [...prevFiles, ...newFiles]);
-    setErrorMessage(null);
-  };
+    router.push("/");
+  }, [files]);
 
-  const handleRemoveFile = (index: number) => {
-    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
-  };
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/png": [".png"],
+      "image/jpeg": [".jpg", ".jpeg"],
+    },
+  });
 
-  const handleSubmit = () => {
-    if (!files.length) {
-      setErrorMessage("Please upload at least 4 images.");
-      return;
-    }
-
-    if (!modelType) {
-      setErrorMessage("Please select a model type.");
-      return;
-    }
-
-    if (files.length < 4) {
-      setErrorMessage("Please upload at least 4 images.");
-      return;
-    }
-
-    setLoading(true);
-    setSuccessMessage(null);
-    setErrorMessage(null);
-
-    // Simulate form submission
-    setTimeout(() => {
-      setLoading(false);
-      setSuccessMessage("Form submitted successfully!");
-    }, 2000);
-  };
+  const modelType = form.watch("type");
 
   return (
-    <Card className="w-full max-w-3xl mx-auto mt-8">
-      <CardHeader>
-        <CardTitle className="text-2xl">
-          Upload Images and Train Model
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <div className="border-2 border-dashed rounded-lg p-4 relative">
-          <div
-            className="text-center"
-            onDragOver={(e) => e.preventDefault()}
-            onDrop={handleFileDrop}>
-            <p className="text-gray-500">Drag & drop images here, or</p>
-            <Button variant="outline" className="mt-2">
-              <Input
-                type="file"
-                accept="image/png, image/jpeg"
-                multiple
-                onChange={handleFileSelect}
-                className="hidden"
-              />
-              Select Images
-            </Button>
-          </div>
-          {errorMessage && (
-            <p className="text-red-500 text-center mt-2">{errorMessage}</p>
-          )}
-          <div className="mt-4 grid grid-cols-2 gap-2">
-            {files.map((file, index) => (
-              <div key={index} className="relative">
-                <img
-                  src={URL.createObjectURL(file)}
-                  className="bg-gray-200 border-2 border-dashed rounded-xl w-full h-24 object-cover"
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="rounded-md flex flex-col gap-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="w-full rounded-md">
+                <FormLabel>Name</FormLabel>
+                <FormDescription>
+                  Give your model a name so you can easily identify it later.
+                </FormDescription>
+                <FormControl>
+                  <Input
+                    placeholder="e.g. Natalie Headshots"
+                    {...field}
+                    className="max-w-screen-sm"
+                    autoComplete="off"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-4">
+            <FormLabel>Type</FormLabel>
+            <FormDescription>
+              Select the type of headshots you want to generate.
+            </FormDescription>
+            <RadioGroup
+              defaultValue={modelType}
+              className="grid grid-cols-3 gap-4"
+              value={modelType}
+              onValueChange={(value) => {
+                form.setValue("type", value);
+              }}>
+              <div>
+                <RadioGroupItem
+                  value="man"
+                  id="man"
+                  className="peer sr-only"
+                  aria-label="man"
                 />
-                <button
-                  className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1"
-                  onClick={() => handleRemoveFile(index)}>
-                  <X className="w-4 h-4" />
-                </button>
+                <Label
+                  htmlFor="man"
+                  className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+                  <FaMale className="mb-3 h-6 w-6" />
+                  Man
+                </Label>
               </div>
-            ))}
+
+              <div>
+                <RadioGroupItem
+                  value="woman"
+                  id="woman"
+                  className="peer sr-only"
+                  aria-label="woman"
+                />
+                <Label
+                  htmlFor="woman"
+                  className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+                  <FaFemale className="mb-3 h-6 w-6" />
+                  Woman
+                </Label>
+              </div>
+              <div>
+                <RadioGroupItem
+                  value="person"
+                  id="person"
+                  className="peer sr-only"
+                  aria-label="person"
+                />
+                <Label
+                  htmlFor="person"
+                  className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-transparent p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary">
+                  <FaRainbow className="mb-3 h-6 w-6" />
+                  Unisex
+                </Label>
+              </div>
+            </RadioGroup>
           </div>
-        </div>
-        <div>
-          <Label htmlFor="model-type" className="text-lg font-semibold">
-            Select Model Type
-          </Label>
-          <RadioGroup
-            defaultValue="man"
-            className="mt-2"
-            value={modelType || undefined}
-            onValueChange={setModelType}>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="man" id="man" />
-              <Label htmlFor="man">Man</Label>
+          <div
+            {...getRootProps()}
+            className=" rounded-md justify-center align-middle cursor-pointer flex flex-col gap-4">
+            <FormLabel>Samples</FormLabel>
+            <FormDescription>
+              Upload 4-10 images of the person you want to generate headshots
+              for.
+            </FormDescription>
+            <div className="outline-dashed outline-2 outline-gray-100 hover:outline-blue-500 w-full h-full rounded-md p-4 flex justify-center align-middle">
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p className="self-center">Drop the files here ...</p>
+              ) : (
+                <div className="flex justify-center flex-col items-center gap-2">
+                  <FaImages size={32} className="text-gray-700" />
+                  <p className="self-center">
+                    Drag 'n' drop some files here, or click to select files.
+                  </p>
+                </div>
+              )}
             </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="woman" id="woman" />
-              <Label htmlFor="woman">Woman</Label>
+          </div>
+          {files.length > 0 && (
+            <div className="flex flex-row gap-4 flex-wrap">
+              {files.map((file) => (
+                <div key={file.name} className="flex flex-col gap-1">
+                  <img
+                    src={URL.createObjectURL(file)}
+                    className="rounded-md w-24 h-24 object-cover"
+                  />
+                  <Button
+                    variant="outline"
+                    size={"sm"}
+                    className="w-full"
+                    onClick={() => removeFile(file)}>
+                    Remove
+                  </Button>
+                </div>
+              ))}
             </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="unisex" id="unisex" />
-              <Label htmlFor="unisex">Unisex</Label>
-            </div>
-          </RadioGroup>
-        </div>
-        <div className="mt-4">
-          <Button onClick={handleSubmit} disabled={loading}>
-            {loading ? "Submitting..." : "Submit"}
+          )}
+
+          <Button type="submit" className="w-full" isLoading={isLoading}>
+            Train Model{" "}
+            {stripeIsConfigured && <span className="ml-1">(1 Credit)</span>}
           </Button>
-        </div>
-        {successMessage && (
-          <p className="text-green-500 mt-2">{successMessage}</p>
-        )}
-        {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
-      </CardContent>
-    </Card>
+        </form>
+      </Form>
+    </div>
   );
 }
