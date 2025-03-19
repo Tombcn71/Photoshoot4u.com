3c31afa906c56f6b0f79d7bccc8a44f38344617b
Tom <tbvanreijn@gmail.com>
2025-03-19 09:39:20 +0100

    tagmanager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,42 +32,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang={locale} dir={dir(locale)} className=" scroll-smooth">
-      <GoogleTagManager
-        gtmId="
-GTM-NZX53VMC"
-      />
-      <Head>
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
-              n.callMethod.apply(n,arguments):n.queue.push   
-              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
-              0;n.version='2.0';n.queue=[];t=b.createElement(e);
-              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,
-              'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', 'YOUR_PIXEL_ID');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=
-            PageView&noscript=1"
-          />
-        </noscript>{" "}
-      </Head>
-      {/* End Meta Pixel Code */}
       <body className="min-h-screen flex flex-col">
-        <PixelTracker />
         <section>
           <Suspense
             fallback={
@@ -83,6 +48,10 @@ GTM-NZX53VMC"
         <Toaster />
         <Analytics />
       </body>
+      <GoogleTagManager
+        gtmId="
+GTM-NZX53VMC"
+      />
     </html>
   );
 }
