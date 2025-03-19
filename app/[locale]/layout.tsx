212b5068c1b9fd4bf7ecedb01b65083bc990240f
Tom <tbvanreijn@gmail.com>
2025-03-19 10:16:39 +0100

    pixel

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,6 +32,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang={locale} dir={dir(locale)} className=" scroll-smooth">
+      {" "}
+      <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+          n.callMethod.apply(n,arguments):n.queue.push   
+          (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+          0;n.version='2.0';n.queue=[];t=b.createElement(e);
+          t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,
+          'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 'NEXT_PUBLIC_YOUR_PIXEL_ID');
+          fbq('track', 'PageView');
+        `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=NEXT_PUBLIC_YOUR_PIXEL_ID&ev=
+        PageView&noscript=1"
+          />
+        </noscript>{" "}
+      </Head>
+      {/* End Meta Pixel Code */}
       <body className="min-h-screen flex flex-col">
         <section>
           <Suspense
@@ -48,10 +79,7 @@ export default function RootLayout({
         <Toaster />
         <Analytics />
       </body>
-      <GoogleTagManager
-        gtmId="
-GTM-NZX53VMC"
-      />
+      <PixelTracker />
     </html>
   );
 }
--- components/PixelTracker.ts
+++ components/PixelTracker.ts
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import ReactPixel from "react-facebook-pixel";
 const PixelTracker = () => {
   useEffect(() => {
-    const pixelId = "YOUR_PIXEL_ID";
+    const pixelId = "NEXT_PUBLIC_YOUR_PIXEL_ID";
     ReactPixel.init(pixelId);
     ReactPixel.pageView();
   }, []);
