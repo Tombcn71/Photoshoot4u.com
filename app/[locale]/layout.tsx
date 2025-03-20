e175ef7a774da3229b09f2b79f0405f236933b95
Tom <tbvanreijn@gmail.com>
2025-03-20 12:28:34 +0100

    banner

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import dynamic from "next/dynamic";
 import i18nConfig from "@/i18nConfig";
 import { dir } from "i18next";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Banner from "@/components/Banner";
 
 export const metadata = {
   title: "Professional AI Headshots in minutes | Photoshoot4u",
@@ -44,6 +45,7 @@ GTM-NZX53VMC"
             fallback={
               <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
             }>
+            <Banner />
             <Navbar />
             <CrispWithNoSSR />
           </Suspense>
--- components/Hero5.tsx
+++ components/Hero5.tsx
@@ -15,20 +15,19 @@ export default () => {
   ];
 
   return (
-    <div className="flex flex-col items-center pt-16">
-      <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
-        <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
-          <h1 className="text-5xl font-bold"> {t("header")}✨</h1>
+    <div className="flex flex-col items-center pt-4 lg:pt-16">
+      <div className="flex flex-col lg:flex-row items-center gap-4 lg:gap-8 p-4 lg:p-8 max-w-6xl w-full">
+        <div className="flex flex-col space-y-3 lg:space-y-4 lg:w-1/2 w-full">
+          <h1 className="text-5xl font-bold">{t("header")}✨</h1>
           <p className="text-lg">{t("subheader")}</p>
 
           <div className="flex flex-col space-y-2">
             <Link href="/login">
-              {" "}
               <Button className="w-full lg:w-1/2">{t("button")}</Button>
             </Link>
             <p className="text-sm text-gray-500 italic">
               Trusted by professionals worldwide. Quick and efficient.
-            </p>{" "}
+            </p>
             <div className="mt-4 text-gray-500">
               <span>Already a member? </span>
               <Link className="text-blue-600 hover:underline" href="/login">
@@ -37,7 +36,7 @@ export default () => {
             </div>
           </div>
         </div>
-        <div className="lg:w-1/2 w-full mt-8 lg:mt-0">
+        <div className="lg:w-1/2 w-full mt-6 lg:mt-0">
           <img
             src={Heroic.src}
             alt="AI Headshot Illustration"
